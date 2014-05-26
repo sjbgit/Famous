fbb15563bd44c59910db22fd43428698c55f93e2
sjbgit <sjb_strat@hotmail.com>
2014-05-26 05:12:57 -0500

    require config

--- Angular/test/app/src/requireConfig.js
+++ Angular/test/app/src/requireConfig.js
@@ -7,7 +7,11 @@ require.config({
         famous: '../lib/famous',
         requirejs: '../lib/requirejs/require',
         almond: '../lib/almond/almond',
-        'famous-polyfills': '../lib/famous-polyfills/index'
-    }
+        'famous-polyfills': '../lib/famous-polyfills/index',
+        'famous-angular': '../lib/famous-angular/dist/famous-angular'
+    },
+    packages: [
+
+    ]
 });
 require(['main']);
